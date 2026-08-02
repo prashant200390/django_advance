7f26c19c8517fba42812772c0f48e75777d1b831
prashant200390 <prajwalofficial90@gmail.com>
2026-08-02 17:29:28 +0545

    CRUD - update(edit)_ModelForms

---
+++ 03_mdoelforms-update(edit)/student/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import studentForm
+from .models import Student
+# Create your views here.
+
+def student_create(request):
+    form = studentForm()
+    if request.method == 'POST':
+        form = studentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'student_success.html')
+    return render(request,'student_form.html',{'form':form})
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request,'student_list.html',{'students':students})
+
+def student_detail(request,pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request,'student_detail.html',{'student':student})
+
+def student_edit(request,pk):
+    student = get_object_or_404(Student,pk=pk)
+    if request.method == "POST":
+        form = studentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = studentForm(instance=student)
+    return render(request,'student_form.html',{'form':form})
